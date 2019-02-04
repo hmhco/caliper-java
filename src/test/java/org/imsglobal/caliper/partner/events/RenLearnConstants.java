27a27f7c05e866fe77a2229ce575a5589d481a93
Damodaran, Vibin <Vibin.Damodaran@hmhco.com>
2019-02-04 14:48:46 -0500

    HRI-63 - RenLearn Partner Service Star score event caliper events test case

---
+++ src/test/java/org/imsglobal/caliper/partner/events/RenLearnConstants.java
@@ -0,0 +1,9 @@
+package org.imsglobal.caliper.partner.events;
+
+import org.imsglobal.caliper.ri.events.HMHConstants;
+
+public class RenLearnConstants extends HMHConstants{
+
+    public static final String APP_REN_LEAN_NAME = "PartnerService";
+    
+}
